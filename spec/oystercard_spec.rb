5d02d3043ab19967801883a905952458eec01835
Kirk Smith <kirks@hotmail.co.uk>
2015-11-30 15:40:34 +0000

    #4

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,10 @@
 require 'oystercard'
 
 describe Oystercard do
+  subject(:oystercard){described_class.new}
+
+  it 'is expected to initialise with balance 0 ' do
+    expect(oystercard.balance).to eq 0 
+  end
 
 end
