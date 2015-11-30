5e580491a8aed270a56a30a903630081045da1fd
michaellennox <michaelctlennox@gmail.com>
2015-11-30 18:18:06 +0000

    Completed challenge #09

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,6 +2,7 @@ class Oystercard
 
   attr_reader :balance
   CARD_CAP = 90
+  MIN_BALANCE = 1
 
   def initialize
     @balance = 0
@@ -22,6 +23,7 @@ class Oystercard
   end
 
   def touch_in
+    fail "Balance too low, please top up" if balance_too_low?
     @traveling = true
   end
 
@@ -35,6 +37,10 @@ class Oystercard
 
   def exceed_cap?(amount)
     balance + amount > CARD_CAP
+  end
+
+  def balance_too_low?
+    balance < MIN_BALANCE
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,57 +7,78 @@ describe Oystercard do
     expect(oystercard.balance).to eq 0
   end
 
-  describe '#top_up' do
+  context 'while card has a working balance' do
 
-    it 'is expected to return the argument passed plus current balance' do
-      expect(oystercard.top_up(5)).to eq 5
+    before { oystercard.top_up(10) }
+
+    describe '#top_up' do
+
+      it 'is expected to add the argument passed to the balance variable' do
+        expect(oystercard.balance).to eq 10
+      end
+
     end
 
-    it 'is expected to add the argument passed to the balance variable' do
-      oystercard.top_up(5)
-      expect(oystercard.balance).to eq 5
+    describe '#deduct' do
+
+      it 'is expected to return the current balance minus argument passed' do
+        expect(oystercard.deduct(5)).to eq 5
+      end
+
+      it 'is expected to reduce current balance by argument passed' do
+        expect{ oystercard.deduct(8) }.to change{ subject.balance }.by (-8)
+      end
+
     end
 
-    it 'is expected to raise error if topup will raise balance over cap' do
-      oystercard.top_up(Oystercard::CARD_CAP)
-      cap_exceeded = "Cap exceeded £#{Oystercard::CARD_CAP}"
-      expect { oystercard.top_up(1) }.to raise_error cap_exceeded
+    describe '#touch_in' do
+
+      it 'is expected to change in_journey? to true' do
+        oystercard.touch_in
+        expect(oystercard).to be_in_journey
+      end
+
+    end
+
+    describe '#touch_out' do
+      it 'is expected to change in_journey? to false' do
+        oystercard.touch_in
+        oystercard.touch_out
+        expect(oystercard).not_to be_in_journey
+      end
     end
 
   end
 
-  describe '#deduct' do
+  context 'while card does not have a working balance' do
+    describe '#top_up' do
 
-    it 'is expected to return the current balance minus argument passed' do
-      oystercard.top_up(10)
-      expect(oystercard.deduct(5)).to eq 5
+      it 'is expected to return the argument passed plus current balance' do
+        expect(oystercard.top_up(5)).to eq 5
+      end
+
+      it 'is expected to raise error if topup will raise balance over cap' do
+        oystercard.top_up(Oystercard::CARD_CAP)
+        cap_exceeded = "Cap exceeded £#{Oystercard::CARD_CAP}"
+        expect { oystercard.top_up(1) }.to raise_error cap_exceeded
+      end
+
     end
 
-    it 'is expected to reduce current balance by argument passed' do
-      oystercard.top_up(25)
-      expect{ oystercard.deduct(8) }.to change{ subject.balance }.by -8
+    describe '#in_journey?' do
+
+      it 'is in_journey? false on initialisation' do
+        expect(oystercard.in_journey?).to be false
+      end
+
     end
 
-  end
+    describe '#touch_in' do
 
-  describe '#in_journey?' do
-    it 'is in_journey? false on initialisation' do
-      expect(oystercard.in_journey?).to be false
-    end
-  end
+      it 'is expected to raise an error if balance is below £1 at time of touch in' do
+        expect{ oystercard.touch_in }.to raise_error "Balance too low, please top up"
+      end
 
-  describe '#touch_in' do
-    it 'is expected to change in_journey? to true' do
-      oystercard.touch_in
-      expect(oystercard).to be_in_journey
-    end
-  end
-
-  describe '#touch_out' do
-    it 'is expected to change in_journey? to false' do
-      oystercard.touch_in
-      oystercard.touch_out
-      expect(oystercard).not_to be_in_journey
     end
   end
 
