e5e87ea27d5630cbf0d4ae069e14abc1860dd712
michaellennox <michaelctlennox@gmail.com>
2015-12-01 10:20:50 +0000

    Fixed spec indent, added README

---
+++ README.md
@@ -0,0 +1,9 @@
+# OysterCard Challenge
+
+This is our working solution to the [Oystercard Challenge](https://github.com/makersacademy/course/tree/master/oystercard)
+
+## Contributors
+
+* [Colin McCulloch](https://github.com/ColinMcCulloch)
+* [Andrew Burnie](https://github.com/Andrew47)
+* [Michael Lennox](https://github.com/michaellennox) michael@michaellennox.me
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,11 +6,12 @@ describe Oystercard do
       expect(subject.balance).to eq 0
     end
   end
-describe 'top_up' do
-  it 'adds cash to the oystercard' do
-    subject.top_up(10)
-    expect(subject.balance).to eq 10
+
+  describe 'top_up' do
+    it 'adds cash to the oystercard' do
+      subject.top_up(10)
+      expect(subject.balance).to eq 10
+    end
   end
-end
 
 end
