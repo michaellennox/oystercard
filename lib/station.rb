ba782a1a1bb315c41badfe984bac8f423673574e
Andrew Burnie <apburnie@hotmail.co.uk>
2015-12-01 16:19:59 +0000

    Stage 13 solved using Station as a class

--- lib/station.rb
+++ lib/station.rb
@@ -1,6 +1,9 @@
 class Station
+  attr_reader :zone, :name
 
   def initialize (station, zone)
+    @zone = zone
+    @name = station
   end
 
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -4,6 +4,16 @@ describe Station do
 
   subject(:station) { Station.new :station, 1}
 
-  it {is_expected.to respond_to(:zone)}
+  describe '#zone' do
+    it 'should give the zone of the station' do
+      expect(station.zone).to eq 1
+    end
+  end
+
+  describe '#name' do
+    it 'should give the name of the station' do
+      expect(station.name).to eq :station
+    end
+  end
 
 end
