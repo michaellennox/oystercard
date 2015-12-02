ba41a8e2d234c4d50dde8ace2814992bc9909a43
Katie Smith <kls.smith@yahoo.co.uk>
2015-12-02 15:28:10 +0000

    Started to extract journey class

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,4 +1,15 @@
 class Journey
+  attr_reader :current_journey
 
-  
+  def set_exit(station)
+    @current_journey[:exit_station] = station
+  end
+
+  def touch_out(station)
+    set_exit(station)
+    @journey_history << current_journey
+    @current_journey = {}
+    deduct(MINIMUM_FARE)
+  end
+
 end
--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,8 +1,11 @@
+require_relative 'journey'
+
 class Oystercard
   MAXIMUM_BALANCE = 90
   MINIMUM_FARE = 1
 
   attr_reader :balance, :journey_history, :current_journey
+  attr_accessor :journey
 
   def initialize
     @balance = 0
@@ -17,35 +20,28 @@ class Oystercard
 
   def touch_in(station)
     fail "Insufficent funds: top up" if balance < MINIMUM_FARE
+    start_new_journey
     set_entry(station)
   end
 
   def touch_out(station)
-    set_exit(station)
+    journey.set_exit(station)
     @journey_history << current_journey
     @current_journey = {}
     deduct(MINIMUM_FARE)
   end
 
+  def start_new_journey
+   @journey = Journey.new
+  end
 
   private
-
-  def in_journey?
-    !!@current_journey[:entry_station]
-
-  end
-
-  def set_exit(station)
-    @current_journey[:exit_station] = station
-  end
 
   def set_entry(station)
     @current_journey[:entry_station] = station
   end
 
-
   def deduct(value)
     @balance -= value
   end
-
 end
