7a43587a22e295f3d251a17d5023d0fb69f9184b
michaellennox <michaelctlennox@gmail.com>
2015-12-03 15:16:09 +0000

    Wrote failing test for start_journey functionality

--- lib/journeylog.rb
+++ lib/journeylog.rb
@@ -2,8 +2,11 @@ require 'journey'
 
 class JourneyLog
 
+  attr_reader :history
+
   def initialize(journey: Journey)
     @journey_klass = journey
+    @history = []
   end
 
 end
--- spec/journeylog_spec.rb
+++ spec/journeylog_spec.rb
@@ -2,10 +2,19 @@ require 'journeylog'
 
 describe JourneyLog do
 
-  subject(:journeylog) { described_class.new }
+  subject(:journeylog) { described_class.new(journey: journey) }
+  let(:journey) { double :journey }
+  let(:station1) { double :station }
 
   it 'should initialize with an empty array to log the journies' do
-    expect (journeylog.history).to eq []
+    expect(journeylog.history).to eq []
+  end
+
+  describe '#start_journey' do
+    it 'should start a new journey' do
+      journeylog.start_journey(station1)
+      expect(journeylog.current_journey).to
+    end
   end
 
 end
