0770ec13aef4660472961c61fe17260721960bd6
Katie Smith <kls.smith@yahoo.co.uk>
2015-12-03 16:26:54 +0000

    Start journed and new journey methods made for journey log

--- lib/journeylog.rb
+++ lib/journeylog.rb
@@ -2,11 +2,19 @@ require 'journey'
 
 class JourneyLog
 
-  attr_reader :history
+  attr_reader :history, :current_journey
 
   def initialize(journey: Journey)
     @journey_klass = journey
     @history = []
   end
 
+  def new_journey
+    @current_journey = @journey_klass.new
+  end
+
+  def start_journey(station)
+    new_journey
+    current_journey.set_entry(station)
+  end
 end
--- spec/journeylog_spec.rb
+++ spec/journeylog_spec.rb
@@ -2,7 +2,8 @@ require 'journeylog'
 
 describe JourneyLog do
 
-  subject(:journeylog) { described_class.new(journey: journey) }
+  subject(:journeylog) { described_class.new(journey: journeyinit) }
+  let(:journeyinit) { double :journey, new: journey }
   let(:journey) { double :journey }
   let(:station1) { double :station }
 
@@ -10,11 +11,17 @@ describe JourneyLog do
     expect(journeylog.history).to eq []
   end
 
-  describe '#start_journey' do
+  describe '#new_journey' do
     it 'should start a new journey' do
-      journeylog.start_journey(station1)
-      expect(journeylog.current_journey).to
+      expect(journeyinit).to receive(:new)
+      journeylog.new_journey
     end
   end
 
+  describe '#start_journey' do
+    it 'should start a new journey and give it an entry station' do
+      expect(journey).to receive(:set_entry).with(station1)
+      journeylog.start_journey(station1)
+    end
+  end
 end
